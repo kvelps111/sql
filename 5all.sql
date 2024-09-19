efce3c662803c9828063254d0dc669ad65ae6410
Kristers <Niks@gmail.com>
2024-09-19 09:05:00 +0300

    all join

---
+++ 5all.sql
@@ -0,0 +1,20 @@
+use sql_store;
+SELECT 
+    c.first_name,
+    c.last_name,
+    c.customer_id,
+    o.order_id,
+    o.order_date,
+    o.order_statuses,
+    sh.name
+FROM
+    customers c
+        JOIN
+    orders o USING (customer_id)
+        JOIN
+    shippers sh USING (shipper_id)
+		JOIN 
+    order_statuses os on o.status = os.order_status_id, name ;
+    
+    
+
